5767d36f01a5c8ee4143df59bf5927fd3d122e00
jsomerville <jesse.somerville@praetorian.com>
2018-12-02 19:47:27 -0600

    Basic visualization finished

--- analyze.go
+++ analyze.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/zmb3/spotify"
 	// "golang.org/x/oauth2"
 )
+
+type analysisData struct {
+	tempo float64
+}
 
 func analyzePlaylist(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
@@ -23,14 +26,16 @@ func analyzePlaylist(w http.ResponseWriter, r *http.Request) {
 		playlists, err := client.CurrentUsersPlaylists()
 		HandleError(w, err)
 
+		var pl spotify.SimplePlaylist
 		var tracks []spotify.PlaylistTrack
 		for i := range playlists.Playlists {
 			if playlists.Playlists[i].Name == playlistName {
 				tracks = getTracks(playlists.Playlists[i], client, w)
+				pl = playlists.Playlists[i]
 			}
 		}
 
-		analyzeTracks(tracks, w, client)
+		analyzeTracks(pl, tracks, w, client)
 	} else {
 		http.Redirect(w, r, "/home", http.StatusFound)
 	}
@@ -70,9 +75,28 @@ func getTracks(pl spotify.SimplePlaylist, client spotify.Client, w http.Response
 	return tracks
 }
 
-func analyzeTracks(tracks []spotify.PlaylistTrack, w http.ResponseWriter, client spotify.Client) {
-	//firstTrack := getFirstTrack(tracks, w)
-	audioAnalysis(tracks, w, client)
+func analyzeTracks(pl spotify.SimplePlaylist, tracks []spotify.PlaylistTrack, w http.ResponseWriter, client spotify.Client) {
+	firstTrack := getFirstTrack(tracks, w)
+	data := audioAnalysis(tracks, w, client)
+
+	t, err := time.Parse(time.RFC3339, firstTrack.AddedAt)
+	HandleError(w, err)
+	firstTrack.AddedAt = t.Format(time.RFC822)
+
+	a := struct {
+		Playlist spotify.SimplePlaylist
+		FirstTrack spotify.PlaylistTrack
+		Tempo, BPS float64
+	} {
+		pl,
+		firstTrack,
+		data.tempo,
+		60.0 / data.tempo,
+	}
+
+	err = tpl.ExecuteTemplate(w, "analyze.gohtml", a)
+	HandleError(w, err)
+
 }
 
 func getFirstTrack(tracks []spotify.PlaylistTrack, w http.ResponseWriter) spotify.PlaylistTrack {
@@ -89,26 +113,27 @@ func getFirstTrack(tracks []spotify.PlaylistTrack, w http.ResponseWriter) spotif
 	return firstTrack
 }
 
-func audioAnalysis(tracks []spotify.PlaylistTrack, w http.ResponseWriter, client spotify.Client) {
+func audioAnalysis(tracks []spotify.PlaylistTrack, w http.ResponseWriter, client spotify.Client) analysisData {
 	var aa []*spotify.AudioAnalysis
 
 	for i := range tracks {
-		fmt.Fprintln(w, i)
 		a, err := client.GetAudioAnalysis(tracks[i].Track.SimpleTrack.ID)
 		aa = append(aa, a)
 		HandleError(w, err)
 	}
 
-	var tempo, loudness float64
+	var tempo float64
 	for i := range aa {
 		tempo += aa[i].Track.Tempo
-		loudness += aa[i].Track.Loudness
 	}
 
 	tempo = tempo / float64(len(tracks))
-	loudness = loudness / float64(len(tracks))
 
-	fmt.Fprintf(w, "Average Tempo: %.2f\n", tempo)
-	fmt.Fprintf(w, "Average Loudness: %.2f\n", loudness)
+	data := analysisData {
+		tempo,
+	}
+
+	return data
+
 }
 
